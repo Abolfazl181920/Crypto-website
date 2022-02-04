5db69d94ef4b3d1c242913d1471a8db90e4a8389
Abolfazl <abolfazlm181920@gmail.com>
2022-02-04 19:51:19 +0330

    i wow & scroll & done Nav

--- src/Components/Navigation/Nav.js
+++ src/Components/Navigation/Nav.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import WOW from 'wowjs';
 
 const Nav = () => {
+
+    useEffect(() => {
+        new WOW.WOW().init();
+    });
+
     return (
         <nav>
             <p className="description">Send payment <br /> requests, get paid <br /> fast!</p>
             <p className="mini_description">86% of our payment requests get paid within 12 hours.</p>
         </nav>
-    )
+    );
 }
 
 export default Nav;
