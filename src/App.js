3ee13a88c41805f13c02b8f93832382e2e63942d
Abolfazl <abolfazlm181920@gmail.com>
2022-02-04 18:59:56 +0330

    done Nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Header from 'Components/Header/Header';
+import Nav from 'Components/Navigation/Nav';
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Nav />
     </div>
   );
 }
---
+++ src/Components/Navigation/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <nav>
+            <p className="description">Send payment <br /> requests, get paid <br /> fast!</p>
+            <p className="mini_description">86% of our payment requests get paid within 12 hours.</p>
+        </nav>
+    )
+}
+
+export default Nav;
