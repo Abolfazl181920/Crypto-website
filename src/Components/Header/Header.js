b5d7b22abef04a555bf0ebbc56787d91f4500c25
Abolfazl <abolfazlm181920@gmail.com>
2022-02-04 18:40:21 +0330

    done header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,8 +6,8 @@ const Header = () => {
     return (
         <header className="header">
             <div className="header_links" style={{ zIndex: '10', position: 'absolute' }}>
-                {/* <a>contact</a>
-                <a>faq</a> */}
+                <a>contact</a>
+                <a>faq</a>
                 <button>Create account</button>
             </div>
             <h2 className="brand" style={{ position: 'absolute', zIndex: '10' }}>Crypto</h2>
