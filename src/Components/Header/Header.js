99156ebb5f1560618b780bdc69a162b67cc7bc0e
Abolfazl <abolfazlm181920@gmail.com>
2022-02-04 18:22:57 +0330

    done button

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,9 +6,9 @@ const Header = () => {
     return (
         <header className="header">
             <div className="header_links" style={{ zIndex: '10', position: 'absolute' }}>
+                {/* <a>contact</a>
+                <a>faq</a> */}
                 <button>Create account</button>
-                <a>contact</a>
-                <a>faq</a>
             </div>
             <h2 className="brand" style={{ position: 'absolute', zIndex: '10' }}>Crypto</h2>
             <video className="fullscreen-video" id="myVideo" autoplay="autoplay" muted loop>
