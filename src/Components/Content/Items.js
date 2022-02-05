2c91fdc17802243226c4d97d284c665cb308d6dd
Abolfazl <abolfazlm181920@gmail.com>
2022-02-05 20:53:35 +0330

    done Items & styles

--- src/Components/Content/Items.js
+++ src/Components/Content/Items.js
@@ -2,25 +2,25 @@ import React from 'react';
 
 const Items = () => {
     return (
-        <div>
+        <div className="items">
             <div>
                 <h3>One-off or resuable</h3>
-                <p>Request one payment. Or get paid multiple with a resuable link.</p>
+                <p>Request one payment. Or get paid <br /> multiple with a resuable link.</p>
             </div>
 
             <div>
                 <h3>One-off or resuable</h3>
-                <p>Request one payment. Or get paid multiple with a resuable link.</p>
+                <p>Request one payment. Or get paid <br /> multiple with a resuable link.</p>
             </div>
 
             <div>
                 <h3>One-off or resuable</h3>
-                <p>Request one payment. Or get paid multiple with a resuable link.</p>
+                <p>Request one payment. Or get paid <br /> multiple with a resuable link.</p>
             </div>
 
             <div>
                 <h3>One-off or resuable</h3>
-                <p>Request one payment. Or get paid multiple with a resuable link.</p>
+                <p>Request one payment. Or get paid <br /> multiple with a resuable link.</p>
             </div>
         </div>
     );
