86cfb1b8a4d1434d75c74ca0ace42bde3933fa23
Abolfazl <abolfazlm181920@gmail.com>
2022-02-06 21:57:54 +0330

    add styles of Scan.js

--- src/Components/Content/Scan.js
+++ src/Components/Content/Scan.js
@@ -4,7 +4,7 @@ function Scan() {
     return (
         <>
             <h3 className="scan">Scan <br /> & Go</h3>
-            <p className="scan_description"></p>
+            <p className="scan_description">Transform your payment link into a QR code that customers can scan with their <br /> phone to pay.</p>
         </>
     )
 }
