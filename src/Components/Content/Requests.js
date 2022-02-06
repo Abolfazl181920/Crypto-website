2649604f15f2ba4b6934d733fdc6008323387ecf
Abolfazl <abolfazlm181920@gmail.com>
2022-02-06 21:23:07 +0330

    done request part

--- src/Components/Content/Requests.js
+++ src/Components/Content/Requests.js
@@ -8,6 +8,8 @@ const Requests = () => {
             <video className="request_video" autoplay="autoplay" muted loop>
                 <source src={Request} />
             </video>
+            <h3>Easily <br /> send requests <br /> via e-mail or SMS</h3>
+            <p>... or copy-paste the link</p>
         </section>
     );
 }
