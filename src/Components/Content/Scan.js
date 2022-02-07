c1ea1233c9db4c6fe392e9bcdf57c685a4c08626
Abolfazl <abolfazlm181920@gmail.com>
2022-02-07 20:07:08 +0330

    solve the bug of pointer

--- src/Components/Content/Scan.js
+++ src/Components/Content/Scan.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import QR from 'Assests/QR.png';
 
 const Scan = () => {
+
+    const [ cursorX, setCursorX ] = useState();
+    const [ cursorY, setCursorY ] = useState();
+
+    window.addEventListener('mousemove', (e) => {
+        setCursorX(e.pageX);
+        setCursorY(e.pageY);
+    });
+    
     return (
         <>
             <h3 className="scan">Scan <br /> & Go</h3>
             <p className="scan_description">Transform your payment link into a QR code that customers can scan with their <br /> phone to pay.</p>
+            <img src={QR} alt="QR code" />
         </>
-    )
+    );
 }
 
 export default Scan;
