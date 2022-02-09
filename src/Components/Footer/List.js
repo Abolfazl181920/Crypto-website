4cfdb22be6bf4ee53d0f7c69418815f55bec88d9
Abolfazl <abolfazlm181920@gmail.com>
2022-02-09 18:35:31 +0330

    delete refs from links

--- src/Components/Footer/List.js
+++ src/Components/Footer/List.js
@@ -5,9 +5,9 @@ const List = () => {
         <div className="dropdown">
             <button className="drop_button">Dropdown</button>
             <div className="dropdown_content">
-                <a href="#home">Home</a>
-                <a href="#about">About</a>
-                <a href="#contact">Contact</a>
+                <a>Home</a>
+                <a>About</a>
+                <a>Contact</a>
             </div>
         </div>
     );
