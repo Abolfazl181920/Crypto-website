7a47bef4e838486c4fa47fe3205ba8708ac3f10d
Abolfazl <abolfazlm181920@gmail.com>
2022-02-09 18:01:18 +0330

    set h2 from footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,13 +5,13 @@ import NavAnim from 'Assests/loop.webm';
 const Footer = () => {
     return (
         <footer>
-            <h2>Start for free today</h2>
+            <h2>Start for <br /> free today</h2>
             <button>Login now</button>
             <video className="footer_video" autoplay="autoplay" muted loop>
                 <source src={NavAnim} />
             </video>
         </footer>
-    )
+    );
 }
 
 export default Footer;
