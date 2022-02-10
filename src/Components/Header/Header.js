ddc07f9a19459e55fd8dde049b1db830a18c8505
Abolfazl <abolfazlm181920@gmail.com>
2022-02-10 23:39:13 +0330

    add filter mode to all buttons

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
-
 import NavAnim from 'Assests/loop.webm';
 
 const Header = () => {
     return (
         <header className="header">
             <div className="header_links" style={{ zIndex: '10', position: 'fixed' }}>
-                <Link to="faq">FAQ</Link>
-                <Link to="contact">contact</Link>
+                <a>FAQ</a>
+                <a>contact</a>
                 <button>Create account</button>
             </div>
             <h2 style={{ position: 'fixed', zIndex: '10' }}>Crypto</h2>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-import App from './App';
-
+import App from 'App';
 import './Styles/App.css';
 
 ReactDOM.render(
