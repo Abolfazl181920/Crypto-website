b603d309e27c65524a87577c52f350b8858df7cf
Abolfazl <abolfazlm181920@gmail.com>
2022-02-10 18:19:29 +0330

    solve 1  bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
 import Header from 'Components/Header/Header';
 import Nav from 'Components/Navigation/Nav';
 import Content from 'Components/Content/Content';
@@ -7,18 +9,26 @@ import ScanPointer from 'Components/Content/ScanPointer';
 import Methods from 'Components/Methods/Methods';
 import Footer from 'Components/Footer/Footer';
 
+import Faq from 'Components/Faq/Faq';
+import Contact from 'Components/Contact/Contact';
 
 const App = () => {
-  return (
-    <div>
-      <Header />
-      <Nav />
-      <Content />
-      <ScanPointer />
-      <Methods />
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				<Header />
+				<Nav />
+				<Content />
+				<ScanPointer />
+				<Methods />
+				<Footer />
+				<Routes>
+					<Route path="/faq" element={ <Faq /> } />
+					<Route path="/contact" element={ <Contact /> } />
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
