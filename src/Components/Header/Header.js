e294370b1c73ce2c5c7dbc5d38a5ad90a6583be2
Abolfazl <abolfazlm181920@gmail.com>
2022-02-12 18:40:37 +0330

    remove textDec of links

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import NavAnim from 'Assests/loop.webm';
 
@@ -6,8 +7,8 @@ const Header = () => {
     return (
         <header className="header">
             <div className="header_links" style={{ zIndex: '10', position: 'fixed' }}>
-                <a href="../Faq/Faq.js">FAQ</a>
-                <a href="../Contact/Contact.js">contact</a>
+                <Link to="/faq" style={{ textDecoration: 'none' }}>FAQ</Link>
+                <Link to="/contact" style={{ textDecoration: 'none' }}>contact</Link>
                 <button>Create account</button>
             </div>
             <h2 style={{ position: 'fixed', zIndex: '10' }}>Crypto</h2>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// import App from 'App';
 import Home from 'Home';
 import './Styles/App.css';
 
