a14a88ab64910665da90fa06e291e2068f36e296
Abolfazl <abolfazlm181920@gmail.com>
2022-02-13 20:07:21 +0330

    delete Json & map on images

--- src/Components/Contact/Members.js
+++ src/Components/Contact/Members.js
@@ -1,15 +1,23 @@
 import React from 'react';
 
-import ImageSource from 'Components/Contact/ImageSource.json';
-
 import Profile from 'Assests/Profile.jpg';
+import Profile2 from 'Assests/Profile2.jpg';
+import Profile3 from 'Assests/Profile3.jpg';
+import Profile4 from 'Assests/Profile4.jpg';
+
+const ImageSource = [
+    { source: Profile, id:1 },
+    { source: Profile2, id:2 },
+    { source: Profile3, id:3 },
+    { source: Profile4, id:4 }
+]
 
 const Members = () => {
     return (
-        <div>         
+        <div>
             {
-                ImageSource.map((source, id) => {
-                    return <img src={source.source} key={id.id} />
+                ImageSource.map(({source, id}) => {
+                    return <img className="profile_img" src={source} key={id} />
                 })
             }
         </div>
