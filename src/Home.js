c091f90dcf4ca072733f5b79879f0d9657dff898
Abolfazl <abolfazlm181920@gmail.com>
2022-02-14 19:39:36 +0330

    add regestration routes

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from 'App';
 import Faq from 'Components/Faq/Faq';
 import Contact from 'Components/Contact/Contact';
+import SignUp from 'Components/SignUp/SignUp';
+import LogIn from 'Components/LogIn/LogIn';
 
 const Home = () => {
     return (
@@ -13,10 +15,12 @@ const Home = () => {
                     <Route path="/" element={ <App /> } exact />
                     <Route path="/faq" element={ <Faq /> } />
                     <Route path="/contact" element={ <Contact /> } />
+                    <Route path="/signup" element={ <SignUp /> } />
+                    <Route path="/login" element={ <LogIn /> } />
                 </Routes>
             </BrowserRouter>
         </div>
-    )
+    );
 }
 
 export default Home;
