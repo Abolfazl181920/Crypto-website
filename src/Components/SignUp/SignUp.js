2fd78dd0803d2e48fe31669c125822d0c2fdebf3
Abolfazl <abolfazlm181920@gmail.com>
2022-02-14 20:13:43 +0330

    add some funcs

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SignUp = () => {
+
+    const [ userName, setUserName ] = useState("");
+    const [ email, setEmail ] = useState("");
+    const [ password, setPassword ] = useState("");
+
+    const handleUserName = (event) => {
+        setUserName(event.target.value);
+    }
+
+    const handleEmail = (event) => {
+        setEmail(event.target.value);
+    }
+
+    const handlePassword = (event) => {
+        setPassword(event.target.value);
+    }
+    
     return (
         <div className="signup_body">
-            <form className="signup_form" action="index.html" method="post">
+            <form className="signup_form" method="post">
                 <h1 className="signup_title">Sign Up</h1>
                 <fieldset>
                     <legend><span class="number">1</span> Your basic info</legend>
                     <label for="name">Name:</label>
-                    <input type="text" id="name" name="user_name" />
+                    <input value={userName} onChange={handleUserName} type="text" id="name" name="user_name" />
 
                     <label for="mail">Email:</label>
-                    <input type="email" id="mail" name="user_email" />
+                    <input value={email} onChange={handleEmail} type="email" id="mail" name="user_email" />
 
                     <label for="password">Password:</label>
-                    <input type="password" id="password" name="user_password" />
+                    <input value={password} onChange={handlePassword} type="password" id="password" name="user_password" />
 
                     <label>Age:</label>
                     <input type="radio" id="under_13" value="under_13" name="user_age" /><label for="under_13" class="light">Under 13</label><br/>
