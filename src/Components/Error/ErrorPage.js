a2436ec27a46e8294b81ea1ef3f83ff1d2edf496
Abolfazl <abolfazlm181920@gmail.com>
2022-02-16 20:09:36 +0330

    navigating

--- src/Components/Error/ErrorPage.js
+++ src/Components/Error/ErrorPage.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { useNavigate, Router } from 'react-router-dom';
 
 import Error404 from 'Assests/404.png';
 
 const ErrorPage = () => {
+
+    let navigate = useNavigate();
+
+    const handleNavigate = () => {
+        if (window.history.length > 1 && document.referrer.includes(window.location.host) !== 1) {
+            navigate(-1);
+        } else {
+            navigate("/");
+        }
+    }
+    
     return(
         <>
             <img src={Error404} className="error404" />
-            <button className="redirect_button">Take me back to HomePage</button>
+            <button onClick={handleNavigate} className="redirect_button">Take me back to HomePage</button>
         </>
     );
 }
