20790dc2272fd8d9bef872f615620299a86d05d1
Abolfazl <abolfazlm181920@gmail.com>
2022-02-20 12:43:56 +0330

    install all dep

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { useTranslation } from 'react-i18next';
+
 const Header = () => {
+
+    const { t, i18n } = useTranslation();
+
     return (
         <header className="header">
             <div className="header_links" style={{ zIndex: '10', position: 'fixed' }}>
                 <Link to="/faq" style={{ textDecoration: 'none' }}>FAQ</Link>
                 <Link to="/contact" style={{ textDecoration: 'none' }}>contact</Link>
                 <Link to="/signup">
-                    <button>Create account</button>
+                    <button>{t ('Create account') }</button>
                 </Link>
             </div>
             <Link to="/">
