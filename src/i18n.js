8d6d1894306983c72082370ec71ffb7eb588f323
Abolfazl <abolfazlm181920@gmail.com>
2022-02-20 12:54:30 +0330

    init i18n component and import it in index again

--- src/i18n.js
+++ src/i18n.js
@@ -1,28 +1,18 @@
 import i18n from "i18next";
+import Backend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from "react-i18next";
 
-const resources = {
-  en: {
-    translation: {
-      "Welcome to React": "Welcome to React and react-i18next"
+i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({
+    fallbackLng: 'en',
+    debug: true,
+    detection: {
+        order: ['queryString', 'cookie'],
+        caches: ['cookie']
+    },
+    interpolation: {
+        escapeValue: false
     }
-  },
-  fr: {
-    translation: {
-      "Welcome to React": "Bienvenue à React et react-i18next"
-    }
-  }
-};
-
-i18n
-  .use(initReactI18next)
-  .init({
-    resources,
-    lng: "en",
-
-interpolation: {
-    escapeValue: false
-}
-});
+})
 
 export default i18n;
